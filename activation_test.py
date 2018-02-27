e2f2c69368160a489bbf3c6b7ec05161bf1c347e
qiubinbin <qiubin780061109@163.com>
2018-02-27 23:04:54 +0800

    Initial commit

---
+++ activation_test.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+x_tensor = torch.linspace(-5, 5, 200)
+x = Variable(x_tensor)
+x_np = x.data.numpy()
+y_sigmoid = F.sigmoid(x).data.numpy()
+y_tanh = F.tanh(x).data.numpy()
+y_softplus = F.softplus(x).data.numpy()
+y_relu = F.relu(x).data.numpy()
+plt.figure(1, figsize=(8, 6))
+plt.subplot(221)
+plt.plot(x_np, y_sigmoid, c='red', label='digmoid')
+plt.ylim((-0.2, 1.2))
+plt.legend(loc='upper left')
+plt.subplot(222)
+plt.plot(x_np, y_tanh, c='green', label='tanh')
+plt.ylim((-1.2, 1.2))
+plt.legend(loc='upper left')
+plt.subplot(223)
+plt.plot(x_np, y_softplus, c='black', label='softplus')
+plt.ylim((-0.2, 6))
+plt.legend(loc='upper left')
+plt.subplot(224)
+plt.plot(x_np, y_relu, c='orange', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='upper left')
+plt.show()
